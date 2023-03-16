47aa3501957425ca7a9b5fd67f474cc8d19f4d10
Nancy Moraa <nancymoraa29@gmail.com>
2023-03-16 04:19:23 -0700

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main- Entry point
  *
- * Return: Always 0 (Success)
+ * Description: A program that prints to standard error
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
+fprintf(stderr,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
