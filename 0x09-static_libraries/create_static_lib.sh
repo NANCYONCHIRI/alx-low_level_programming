95719f31796d693c29d61912a1383da95fb07144
Nancy Onchiri <nancymoraa29@gmail.com>
2023-04-11 06:31:00 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
-=======
 gcc -c *.c
 ar rc liball.a *.c
->>>>>>> 7fb6514dd7148c87afffceae603482a02109dc09
