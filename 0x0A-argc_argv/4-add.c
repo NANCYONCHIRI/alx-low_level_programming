c730ee233cc96c5d5c05c1ef25909d6345a9596c
nancy onchiri <nancymoraa29@gmail.com>
2023-04-12 02:52:25 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,69 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include <ctype.h>
-/**
-* main - adds positive numbers.
-* @argc: number of command line arguments.
-* @argv: array that contains the program command line arguments.
-* Return: 0 - success.
-*/
-int main(int argc, char *argv[])
-{
-int i, j, add = 0;
-
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (!isdigit(argv[i][j]))
-=======
 #include <string.h>
-#include <ctype.h>
-#include "main.h"
+/**
+* check_num - check - string there are digit
+* @str: array str
+*
+* Return: Always 0 (Success)
+*/
+int check_num(char *str)
+{
+/*Declaring variables*/
+unsigned int count;
+
+count = 0;
+while (count < strlen(str)) /*count string*/
+
+{
+if (!isdigit(str[count])) /*check if str there are digit*/
+{
+return (0);
+}
+
+count++;
+}
+return (1);
+}
 
 /**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
-{
-int i, j, length, sum;
-char *ptr;
 
-if (argc < 2)
-printf("0\n");
+{
+
+/*Declaring variables*/
+int count;
+int str_to_int;
+int sum = 0;
+
+count = 1;
+while (count < argc) /*Goes through the whole array*/
+{
+if (check_num(argv[count]))
+
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
+}
+
+/*Condition if one of the number contains symbols that are not digits*/
 else
-{
-sum = 0;
-for (i = 1; i < argc; i++)
-{
-ptr = argv[i];
-length = strlen(ptr);
-
-for (j = 0; j < length; j++)
-{
-if (isdigit(*(ptr + j)) == 0)
->>>>>>> 7fb6514dd7148c87afffceae603482a02109dc09
 {
 printf("Error\n");
 return (1);
 }
-}
-<<<<<<< HEAD
-add += atoi(argv[i]);
-}
-printf("%d\n", add);
-=======
 
-sum += atoi(argv[i]);
+count++;
 }
 
-printf("%d\n", sum);
-}
->>>>>>> 7fb6514dd7148c87afffceae603482a02109dc09
+printf("%d\n", sum); /*print sum*/
+
 return (0);
 }
