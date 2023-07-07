40a0f1a9ef40dfcf6ee61dda57e0a35c94af3bf8
Nancy Onchiri <nancymoraa29@gmail.com>
2023-07-07 07:07:57 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -17,7 +17,7 @@ printf("%u\n", n);
 n = binary_to_uint("1e01");
 printf("%u\n", n);
 n = binary_to_uint("1100010");
-printf("%u\n", n);
+ printf("%u\n", n);
 n = binary_to_uint("0000000000000000000110010010");
 printf("%u\n", n);
 return (0);
